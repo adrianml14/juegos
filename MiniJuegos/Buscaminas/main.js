ed3e317fb8bd751fbc4a62555d21b04c4d9936c0
adrianml14 <adrianml14@educastur.es>
2024-03-08 03:42:19 +0100

    Traduccion buscaminas

--- MiniJuegos/Buscaminas/main.js
+++ MiniJuegos/Buscaminas/main.js
@@ -4,120 +4,117 @@ const tablero = [];
 let vidas = 5;
 
 function inicializar() {
-  vidas = 5; 
-  actualizarVidas(); 
-  generarBotones();
-  for (let i = 0; i < filas; i++) {
-    tablero.push([]);
-    for (let j = 0; j < columnas; j++) {
-      tablero[i].push({ tieneMina: false, destapado: false });
+    vidas = 5; 
+    actualizarVidas(); 
+    generarBotones();
+    for (let i = 0; i < filas; i++) {
+        tablero.push([]);
+        for (let j = 0; j < columnas; j++) {
+            tablero[i].push({ tieneMina: false, destapado: false });
+        }
     }
-   }
 
-  let minas =10;
+    let minas = 10;
 
-  while (minas > 0) {
-    let fila = Math.floor(Math.random() * filas);
-    let columna = Math.floor(Math.random() * columnas);
+    while (minas > 0) {
+        let fila = Math.floor(Math.random() * filas);
+        let columna = Math.floor(Math.random() * columnas);
 
-    if (!tablero[fila][columna].tieneMina) {
-      tablero[fila][columna].tieneMina = true;
-      minas--;
+        if (!tablero[fila][columna].tieneMina) {
+            tablero[fila][columna].tieneMina = true;
+            minas--;
+        }
     }
-  }
 }
 
 function generarBotones() {
-  let html = "";
-  for (let i = 0; i < filas; i++) {
-    html += '<div>';
-    for (let j = 0; j < columnas; j++) {
-      html += '<button type="button" onclick="destapa(event.target, ' + i + ',' + j + ')"></button>';
+    let html = "";
+    for (let i = 0; i < filas; i++) {
+        html += '<div>';
+        for (let j = 0; j < columnas; j++) {
+            html += '<button type="button" onclick="destapa(event.target, ' + i + ',' + j + ')"></button>';
+        }
+        html += '</div>';
     }
-    html += '</div>';
-  }
-  document.querySelector('#matriz').innerHTML = html;
+    document.querySelector('#matriz').innerHTML = html;
 }
 
 function destapa(button, fila, columna) {
-  if (tablero[fila][columna].tieneMina) {
-    vidas--;
-    actualizarVidas(); 
+    if (tablero[fila][columna].tieneMina) {
+        vidas--;
+        actualizarVidas(); 
+        button.style.backgroundColor = 'red';
+        button.disabled = true;
+        alert('¡Había una mina ahí! *explota* \n -1 vida');
 
-    button.style.backgroundColor = 'red'
-    button.disabled = true;
-    alert('¡Habia una mina ahi!  *explota*  \n -1 vida')
-
-    if (vidas === 0) {
-      alert('¡Has perdido! Se acabaron las vidas.');
-      reset();
+        if (vidas === 0) {
+            alert('¡Has perdido! Se acabaron las vidas.');
+            reset();
+        }
+    } else if (minaCercana(fila, columna)) {
+        button.style.backgroundColor = 'yellow';
+        button.disabled = true;
+    } else {
+        button.style.backgroundColor = '#1c7cb4';
+        button.disabled = true;
+        tablero[fila][columna].destapado = true;
     }
-     
-  } else if (minaCercana(fila, columna)) {
-    button.style.backgroundColor = 'yellow';
-    button.disabled = true;
-  } else {
-    button.style.backgroundColor = '#1c7cb4';
-    button.disabled = true;
-    tablero[fila][columna].destapado = true;
-  }
 }
 
 function minaCercana(fila, columna) {
-  for (let i = fila - 1; i <= fila + 1; i++) {
-    for (let j = columna - 1; j <= columna + 1; j++) {
-      if (i >= 0 && j >= 0 && i < filas && j < columnas && (i != fila || j != columna)) {
-        if (tablero[i][j].tieneMina) {
-          return true;
+    for (let i = fila - 1; i <= fila + 1; i++) {
+        for (let j = columna - 1; j <= columna + 1; j++) {
+            if (i >= 0 && j >= 0 && i < filas && j < columnas && (i !== fila || j !== columna)) {
+                if (tablero[i][j].tieneMina) {
+                    return true;
+                }
+            }
         }
-      }
     }
-  }
-  return false;
+    return false;
 }
 
 function actualizarVidas() {
-  document.getElementById('vidas').innerText = 'Vidas: ' + vidas;
-  if (vidas === 0) {
-    setTimeout(function() {
-      alert('¡Has perdido! Se acabaron las vidas.');
-      reset();
-    }, 500); 
-  }
+    document.getElementById('vidas').innerText =  vidas;
+    if (vidas === 0) {
+        setTimeout(function() {
+            alert('¡Has perdido! Se acabaron las vidas.');
+            reset();
+        }, 500); 
+    }
 }
 
 function reset() {
-  tablero.length = 0; 
-  document.querySelector('#matriz').innerHTML = ''; 
-  inicializar();
+    tablero.length = 0; 
+    document.querySelector('#matriz').innerHTML = ''; 
+    inicializar();
 }
 
-inicializar();
-
-
 function mostrarMenuu() {
-  var menuInfo = document.getElementById('menuInfo');
-  menuInfo.style.display = 'block';
-  
-  var botonInfo = document.getElementById('botonInfo');
-  botonInfo.style.display = 'none';
+    var menuInfo = document.getElementById('menuInfo');
+    menuInfo.style.display = 'block';
+    
+    var botonInfo = document.getElementById('botonInfo');
+    botonInfo.style.display = 'none';
 }
 
 function cerrarMenuu() {
-  var menuInfo = document.getElementById('menuInfo');
-  menuInfo.style.display = 'none';
+    var menuInfo = document.getElementById('menuInfo');
+    menuInfo.style.display = 'none';
 
-  var botonInfo = document.getElementById('botonInfo');
-  botonInfo.style.display = 'block';
+    var botonInfo = document.getElementById('botonInfo');
+    botonInfo.style.display = 'block';
 }
 
-//traducciones
-document.getElementById('selector-idiomaM').addEventListener('change', function(){
-  var idiomaSeleccionado = this.value;
+document.addEventListener('DOMContentLoaded', function() {
+  // traducciones
+  document.getElementById('selector-idiomaM').addEventListener('change', function(){
+      var idiomaSeleccionado = this.value;
 
-  document.getElementById('vidas').innerHTML = traduccionesM[idiomaSeleccionado].vidas;
-  document.getElementById('reiniciar').innerHTML = traduccionesM[idiomaSeleccionado].reiniciar;
-  document.getElementById('botonInfo').innerHTML = traduccionesM[idiomaSeleccionado].botonInfo;
-  document.getElementById('txtInfo').innerHTML = traduccionesM[idiomaSeleccionado].txtInfo;
-  document.getElementById('cerrarMenu').innerHTML = traduccionesM[idiomaSeleccionado].cerrarMenu;
-})
+      document.getElementById('txVidas').innerHTML = traduccionesM[idiomaSeleccionado].txVidas;
+      document.getElementById('reiniciar').innerHTML = traduccionesM[idiomaSeleccionado].reiniciar;
+      document.getElementById('botonInfo').innerHTML = traduccionesM[idiomaSeleccionado].botonInfo;
+      document.getElementById('txtInfo').innerHTML = traduccionesM[idiomaSeleccionado].txtInfo;
+      document.getElementById('cerrarMenu').innerHTML = traduccionesM[idiomaSeleccionado].cerrarMenu;
+  });
+});
--- MiniJuegos/Buscaminas/traduccion.js
+++ MiniJuegos/Buscaminas/traduccion.js
@@ -1,31 +1,31 @@
 var traduccionesM = {
 
   "es": {
-      "vidas": "Vidas: 5",
+      "txVidas": "Vidas: ",
       "reiniciar": "Reiniciar",
       "botonInfo": "Información",
       "txtInfo": "<b>¡¡Buenas usuario!! <br> Este juego es un buscaminas simple, <br>pero si destapas un cuadro y es <br>amarillo, ¡cuidado! hay una mina cerca <br>y solo tienes 5 vidas si las pierdes, <br>perderás. También tienes a tu disposición <br>un botón para reiniciar el juego.",
       "cerrarMenu": "Cerrar",
     },
     "en": {
-      "vidas": "Lives: 5",
+      "txVidas": "Lives: ",
       "reiniciar": "Restart",
       "botonInfo": "Information",
       "txtInfo": "<b>Hello user!! <br> This game is a simple minesweeper, <br>but if you uncover a box and it's <br>yellow, be careful! There's a mine nearby <br>and you only have 5 lives, if you lose them, <br>you'll lose. You also have a button to restart the game.",
       "cerrarMenu": "Close",
     },
     "fr": {
-      "vidas": "Vies: 5",
+      "txVidas": "Vies: ",
       "reiniciar": "Redémarrer",
       "botonInfo": "Information",
-      "txtInfo": "<b>Bonjour utilisateur!! <br> Ce jeu est un démineur simple, <br>mais si vous découvrez une case et qu'elle est <br>jaune, attention! Il y a une mine à proximité <br>et vous n'avez que 5 vies, si vous les perdez, <br>vous perdrez. Vous avez également un bouton pour redémarrer le jeu.",
+      "txtInfo": "<b>Bonjour utilisateur!! <br> Ce jeu est un démineur simple, <br>mais si vous découvrez une case et qu'elle est <br>jaune, attention! Il y a une mine à proximité <br>et vous n'avez que 5 vies, si vous les perdez, <br>vous perdrez. Vous avez également un bouton<br>pour redémarrer le jeu.",
       "cerrarMenu": "Fermer",
     },
     "fi": {
-      "vidas": "Elämää: 5",
+      "txVidas": "Elämää: ",
       "reiniciar": "Käynnistä uudelleen",
       "botonInfo": "Tiedot",
-      "txtInfo": "<b>Hei käyttäjä!! <br> Tämä peli on yksinkertainen miinaharava, <br>mutta jos paljastat laatikon ja se on <br>keltainen, ole varovainen! Lähellä on miina <br>ja sinulla on vain 5 elämää, jos menetät ne, <br>häviät. Sinulla on myös nappi pelin uudelleenkäynnistämiseen.",
+      "txtInfo": "<b>Hei käyttäjä!! <br> Tämä peli on yksinkertainen miinaharava, <br>mutta jos paljastat laatikon ja se on <br>keltainen, ole varovainen! Lähellä on miina <br>ja sinulla on vain 5 elämää, jos menetät ne, <br>häviät. Sinulla on myös nappi pelin<br>uudelleenkäynnistämiseen.",
       "cerrarMenu": "Sulje",
     }
 };
